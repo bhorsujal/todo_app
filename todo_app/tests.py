7fda86bd909ccb4a64585498f738d7102089dbd2
Sujal Bhor <bhorsujal@gmail.com>
2024-11-30 16:03:19 +0530

    Add testcases in tests.py under todo_app

--- todo_app/tests.py
+++ todo_app/tests.py
@@ -1,60 +1,47 @@
 from django.test import TestCase
 from django.urls import reverse
-from rest_framework.test import APITestCase
-from rest_framework import status
 from .models import Todo
 
-# Create your tests here.
-
-class TodoAPITestCase(APITestCase):
+class TodoAppTests(TestCase):
     def setUp(self):
-        self.todo1 = Todo.objects.create(title='Task 1', description='Description 1')
-        self.todo2 = Todo.objects.create(title='Task 2', description='Description 2')
+        self.task1 = Todo.objects.create(title="Task 1", description="Description 1")
+        self.task2 = Todo.objects.create(title="Task 2", description="Description 2", isCompleted=False)
 
-    def test_create_todo(self):
-        url = reverse('todo-list')
-        data = {'title': 'New Task', 'description': 'New Description'}
-        response = self.client.post(url, data, format='json') # made one todo object
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Todo.objects.count(), 3) # at setup there were two objects
+    def test_home_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.task1.title)
+        self.assertContains(response, self.task2.title)
+        self.assertFalse(self.task1.isCompleted)
 
-    def test_list_todos(self):
-        url = reverse('todo-list')
-        response = self.client.get(url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 2)
+    def test_create_task_view(self):
+        response = self.client.post(reverse('todo-list'), {'title': 'New Task', 'description': 'New Task Description'})
+        self.assertEqual(response.status_code, 302) # should redirect to home
+        self.assertEqual(Todo.objects.count(), 3)
+        self.assertTrue(Todo.objects.filter(title='New Task').exists())
 
-    def test_get_single_todo(self):
-        url = reverse('todo-detail', kwargs={'pk': self.todo1.pk})
-        response = self.client.get(url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['title'], 'Task 1')
+    def test_update_task_view(self):
+        response = self.client.post(reverse('todo-update', kwargs={'pk': self.task1.pk}))
+        self.assertEqual(response.status_code, 302)
+        self.task1.refresh_from_db()
+        self.assertTrue(self.task1.isCompleted)
 
-    def test_put_todo(self):
-        url = reverse('todo-detail', kwargs={'pk': self.todo2.pk})
-        data = {'title': 'Updated Task 2', 'description': 'Updated Description 2', 'isCompleted': True}
-        response = self.client.put(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.todo2.refresh_from_db()
-        self.assertEqual(self.todo2.title, 'Updated Task 2')
-        self.assertEqual(self.todo2.isCompleted, True)
+    def test_delete_task_view(self):
+        response = self.client.post(reverse('todo-delete', kwargs={'pk': self.task1.pk}))
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Todo.objects.filter(id=self.task1.id).exists())
 
-    def test_patch_todo(self):
-        url = reverse('todo-detail', kwargs={'pk': self.todo2.pk})
-        data = {'title': 'Updated Task 2 (Patch)'}
-        response = self.client.patch(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.todo2.refresh_from_db()
-        self.assertEqual(self.todo2.title, 'Updated Task 2 (Patch)')
-
-    def test_delete_todo(self):
-        url = reverse('todo-detail', kwargs={'pk': self.todo1.pk})
-        response = self.client.delete(url)
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEqual(Todo.objects.count(), 1)
-
-    def test_delete_all_todos(self):
-        url = reverse('todo-delete-all')
-        response = self.client.delete(url)
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+    def test_delete_all_tasks_view(self):
+        response = self.client.post(reverse('todo-delete-all'))
+        self.assertEqual(response.status_code, 302)
         self.assertEqual(Todo.objects.count(), 0)
+
+    def test_update_task_details_view(self):
+        response = self.client.post(reverse('todo-update-details', kwargs={'pk': self.task1.pk}), {
+            'title': 'Updated Title',
+            'description': 'Updated Description'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.task1.refresh_from_db()
+        self.assertEqual(self.task1.title, 'Updated Title')
+        self.assertEqual(self.task1.description, 'Updated Description')
